e60f0df723a4bc5ee78f5da6d93f7fda29fb3fa2
Adidadi0112 <adamgruda1312@gmail.com>
2024-02-05 14:02:45 +0100

    Some UI improvements in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:fasting_app/pages/initial_page.dart';
 import 'package:fasting_app/widgets/stopwatch.dart';
 import 'package:flutter/material.dart';
 
@@ -8,26 +9,117 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // double width = MediaQuery.of(context).size.width;
-    // double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: const Center(
           child: Text(
-            "Pościsz!",
+            "Post przerywany",
             style: TextStyle(color: Colors.white),
           ),
         ),
         backgroundColor: Colors.black,
       ),
+      backgroundColor: Colors.grey.shade200,
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.only(top: 32, left: 8, right: 8),
           child: Column(
             children: [
               Stoper(fastingDuration: fastingDuration),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: ((context) => const InitPage()),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(width * 0.8, height * 0.06),
+                    backgroundColor: Colors.black),
+                child: const Text(
+                  "Zakończ post",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: width * 0.8,
+                height: height * 0.35,
+                child: Card(
+                  color: Colors.grey.shade200,
+                  elevation: 3,
+                    child: const Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "Złote rady podczas postu:",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            size: 8,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Stopniowo wydłużaj post",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            size: 8,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Pij dużo wody",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            size: 8,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Unikaj przetworzonego jedzenia",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            size: 8,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Bądź elastyczny z planem",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+              )
             ],
           ),
         ),
--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -10,6 +10,7 @@ class InitPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Center(
           child: Text(
             "Rozpocznij post!",
